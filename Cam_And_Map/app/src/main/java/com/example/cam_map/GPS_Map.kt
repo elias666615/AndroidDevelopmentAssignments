92713e1139b3207f9b757539830e23b10a20f288
elias666615 <eliasbbb46@gmail.com>
2021-11-03 17:41:09 +0200

    created two new activities and added the navigation from the main activity to these activities

---
+++ Cam_And_Map/app/src/main/java/com/example/cam_map/GPS_Map.kt
@@ -0,0 +1,11 @@
+package com.example.cam_map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GPS_Map : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gps_map)
+    }
+}
