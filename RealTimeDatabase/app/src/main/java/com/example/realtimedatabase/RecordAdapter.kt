fb11454b22368f1e5840c1b1446e26a4e58cb580
elias666615 <eliasbbb46@gmail.com>
2021-11-19 13:49:52 +0200

    ...

--- RealTimeDatabase/app/src/main/java/com/example/realtimedatabase/RecordAdapter.kt
+++ RealTimeDatabase/app/src/main/java/com/example/realtimedatabase/RecordAdapter.kt
@@ -1,4 +1,29 @@
 package com.example.realtimedatabase
 
-class RecordAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecordAdapter(private val dataSet: ArrayList<DiceRecord>): RecyclerView.Adapter<RecordAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val username: TextView = view.findViewById(R.id.tv_rec_username)
+        val dice_numbers: TextView = view.findViewById(R.id.tv_rec_dice)
+        val timestamp: TextView = view.findViewById(R.id.tv_rec_timestamp)
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.record, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        val record = dataSet[position]
+        viewHolder.username.setText(record.username)
+        viewHolder.dice_numbers.setText(record.dice1.toString() + " - " + record.dice2.toString() + " - " + record.dice3.toString() + " - " + record.dice4.toString())
+    }
+
+    override fun getItemCount() = dataSet.size
 }
